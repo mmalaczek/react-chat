c92382a789f0129fed8378184b5cffc37b63a28e
mmalaczek <miroslaw.malaczek@empik.com>
2020-10-19 23:42:01 +0200

    styling css

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Message from "./components/Message";
 
 export default function App() {
     const messages = [
-        { id: 12, user: 'Patryk', content: 'Hej' },
-        { id: 24, user: 'Ania', content: 'Ho' },
-        { id: 67, user: 'Patryk', content: 'Lublin' },
+        { id: 12, user: 'Patryk', content: 'Hej', datetime: Date.now() },
+        { id: 24, user: 'Ania', content: 'Ho', datetime: Date.now() },
+        { id: 67, user: 'Patryk', content: 'Lublin', datetime: Date.now() },
     ];
 
     return (
--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+import './index.css';
+
+function convertDate(timestamp) {
+    const datetime = new Date(timestamp);
+    return `${datetime.getHours()}:${datetime.getMinutes()}:${datetime.getSeconds()}`;
+}
+
 function Message({ message }) {
     return (
         <div className="message">
-            <div className="message--user">{message.user}</div>
+            <div className="message--user">
+                {message.user} <span className="message--time">{convertDate(message.datetime)}</span>
+            </div>
             <div className="message--content">{message.content}</div>
         </div>
     );
